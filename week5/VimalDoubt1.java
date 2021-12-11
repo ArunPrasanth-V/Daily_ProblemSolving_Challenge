523e22c542d606da715ecc737333786d8db5eb41
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2021-12-11 15:05:59 +0530

    Create VimalDoubt1.java

---
+++ week5/VimalDoubt1.java
@@ -0,0 +1,25 @@
+public class MyClass {
+    public static void main(String args[]) {
+    
+        int n=6;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0){
+              System.out.print(i+" ");
+                i++;
+            }
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2!=0)
+            {
+                if(i%2!=0)
+                {
+                    System.out.print(i+" ");
+                    i++;
+                }
+            }
+        }
+    }
+    
+}
