0b0a27f161182349e4df3012d8ca063cb6286e4e
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-01-07 20:02:47 +0530

    Update Find_Smallest_Letter_Greater_Than_Target.java

--- week6/Find_Smallest_Letter_Greater_Than_Target.java
+++ week6/Find_Smallest_Letter_Greater_Than_Target.java
@@ -1,24 +1,17 @@
 //Leetcode Problem No. 744
-
+//   https://leetcode.com/problems/find-smallest-letter-greater-than-target/
 class Solution {
     public char nextGreatestLetter(char[] arr, char target) {
         int start=0,end=arr.length-1;
         while(start<=end)
         {
             int mid=start+(end-start)/2;
-            if(arr[mid]==target)
-            {
-                if(mid==arr.length-1)
-                    return arr[0];
-            }
+           
             if(arr[mid]>target)
                 end=mid-1;
             else
                 start=mid+1;
         }
-     if(start==arr.length)
-            return arr[0];
-     else
-         return arr[start];
+         return arr[start % arr.length];
     }
 }
