e5b1fda9ae7e0e34b6ac7f134883e51f26cdc740
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-01-08 12:33:44 +0530

    Create FindindFloorOfTheNumberArray.java

---
+++ week7/FindindFloorOfTheNumberArray.java
@@ -0,0 +1,28 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int arr[]={1,2,4,5,7,8,11,13,15,16,18};
+      int target=14;
+     System.out.print(binaySearch(arr,target));
+      
+    }
+    static int  binaySearch(int arr[],int target)
+    {
+        int start =0,end=arr.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(arr[mid]>target)
+                  end=mid-1;
+            else if (arr[mid]<target)
+            {
+                start=mid+1;
+            }
+            else
+            {
+                return mid;
+            }
+            
+        }
+        return end;
+    }
+}
