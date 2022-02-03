e23da48c0b548dd0715a87fe3f88113a12c0e2da
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-02-03 07:37:47 +0530

    Update kadanesAlgorithm.java

--- Part9/kadanesAlgorithm.java
+++ Part9/kadanesAlgorithm.java
@@ -1,8 +1,6 @@
 // work both positive and negative integer.
 class Solution {
     public int maxSubArray(int[] nums) {
-        if(nums.length==1)
-            return nums[0];
         int meh=0;
         int msf=Integer.MIN_VALUE;
         for(int i:nums)
