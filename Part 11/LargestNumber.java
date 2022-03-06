7815370df8db1973bba25977781328405b05f2bb
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-03-06 08:09:05 +0530

    Create LargestNumber.java

---
+++ Part 11/LargestNumber.java
@@ -0,0 +1,12 @@
+// 179. Largest Number
+// https://leetcode.com/problems/largest-number/
+class Solution {
+    public String largestNumber(int[] arr) {
+    String result=Arrays.stream(arr)
+			      .mapToObj(String::valueOf)
+			      .sorted((i,j)->(j+i).compareTo(i+j))
+			      .collect(Collectors.joining());
+	return (result.startsWith("0")?"0":result);
+
+    }
+}
