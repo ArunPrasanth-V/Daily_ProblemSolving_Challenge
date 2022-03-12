0e6999e1954247df9a64113714534de6757df11c
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-03-12 19:14:36 +0530

    Update SingleNumber.java

--- Part 11/SingleNumber.java
+++ Part 11/SingleNumber.java
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/single-number/submissions/
 // 136. Single Number
 class Solution {
-    public int singleNumber(int[] nums) {
+    public int singleNumber(int[] nums) 
+    {
      int sum=0;
         for(int i :nums)
         {
