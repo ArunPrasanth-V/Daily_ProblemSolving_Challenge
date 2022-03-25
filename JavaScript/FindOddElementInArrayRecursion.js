89cc2690a218c4252eefc501f0228c1a235c1b3b
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-03-25 06:57:46 +0530

    Pure Recursion

--- JavaScript/FindOddElementInArrayRecursion.js
+++ JavaScript/FindOddElementInArrayRecursion.js
@@ -1,14 +1,15 @@
-oddarray=[]
 function check(arr){ 
+ oddarray=[]
     if(arr.length==0)
-        return;
+        return oddarray;
     if(arr[0]%2!=0)
     {
        oddarray.push(arr[0]);
     }
-    check(arr.slice(1));
+    oddarray=oddarray.concat(check(arr.slice(1)));
+    return oddarray;
 }
 
 const arr=[1,2,3,4,5];
 console.log(check(arr));
-console.log(oddarray);
+
