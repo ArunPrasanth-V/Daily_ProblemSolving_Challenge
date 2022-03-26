93866acf35e86c1eb315f52c31db0723da6c0fe8
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-03-26 11:55:53 +0530

    Create BubbleSort.js

---
+++ JavaScript/BubbleSort.js
@@ -0,0 +1,26 @@
+function sort(arr)
+{
+  for(let i=0;i<arr.length;i++)
+    {
+        let a=0;
+        let isSwap=true;
+        for(let j=1;j<arr.length-i;j++)
+        {
+
+            if(arr[a]>arr[j])
+            {
+                let temp=arr[a];
+                arr[a]=arr[j];
+                arr[j]=temp;
+                isSwap=false;
+            }
+            a++;
+        }
+        if(isSwap)
+                 break;
+    }
+    return arr;
+}
+
+
+console.log(sort([8,5,1,4,3,2,10,7]));
