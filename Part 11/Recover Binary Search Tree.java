072bd87c87f6458a7a373513a459bca95db64efc
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-04-19 21:52:28 +0530

    Create Recover Binary Search Tree.java

---
+++ Part 11/Recover Binary Search Tree.java
@@ -0,0 +1,25 @@
+// 99. Recover Binary Search Tree
+// https://leetcode.com/problems/recover-binary-search-tree/
+class Solution {
+    TreeNode prev = null, first = null, second = null;
+
+    public void recoverTree(TreeNode root) {
+        evalSwappedNodes(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void evalSwappedNodes(TreeNode curr) {
+        if (curr == null)
+            return;
+        evalSwappedNodes(curr.left);
+        if (prev != null && prev.val > curr.val) {
+            if (first == null)
+                first = prev;
+            second = curr;
+        }
+        prev = curr;
+        evalSwappedNodes(curr.right);
+    }
+}
