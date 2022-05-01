cbd0cd20526513ac10eb5a0ea012c01a4529caf4
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-05-01 09:03:57 +0530

    Create PointerExamples.c

---
+++ PointerExamples.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int x=5;
+    int*p=&x;
+    int **q=&p;
+    int ***r=&q;
+    printf("x = %d\n",x);
+    printf("&x = %d\n",&x);
+    printf("&p = %d\n",&p);
+    printf("p = %d\n",p);
+    printf("*p = %d\n",*p);
+    printf("&q = %d\n",&q);
+    printf("q = %d\n",q);
+    printf("*q = %d\n",*q);
+    printf("**q = %d\n",**q);
+    printf("r = %d\n",r);
+    printf("*r = %d\n",*r);
+    printf("**r = %d\n",**r);
+    printf("***r = %d\n",*(*(*r)));
+ return 0;
+}
